635ad5893a287dbc867af86c49872b993c1a5fef
Vinícius Massari <stonefields11@hotmail.com>
2024-10-29 18:56:36 -0300

    :tada: initial commit

---
+++ App.tsx
@@ -0,0 +1,52 @@
+import 'react-native-get-random-values';
+import './src/libs/dayjs';
+
+import { ThemeProvider } from 'styled-components/native';
+import { StatusBar } from 'react-native';
+import { AppProvider, UserProvider } from '@realm/react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+import theme from './src/theme';
+
+import { REALM_APP_ID } from '@env';
+
+import { Routes } from './src/routes';
+import { RealmProvider } from './src/libs/realm';
+
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <SafeAreaProvider style={{ backgroundColor: theme.COLORS.GRAY_800 }}>
+          <StatusBar
+            barStyle='light-content'
+            backgroundColor='transparent'
+            translucent
+          />
+          <UserProvider fallback={SignIn}>
+            <RealmProvider>
+              <Routes />
+            </RealmProvider>
+          </UserProvider>
+        </SafeAreaProvider>
+      </ThemeProvider>
+    </AppProvider>
+  );
+}
---
+++ src/screens/Arrival/index.tsx
@@ -0,0 +1,115 @@
+import { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { X } from 'phosphor-react-native';
+import { BSON } from 'realm';
+
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+import {
+  Container,
+  Content,
+  Description,
+  Footer,
+  Label,
+  LicensePlate,
+  AsyncMessage,
+} from './styles';
+
+import { Historic } from '../../libs/realm/schemas/Historic';
+import { useObject, useRealm } from '../../libs/realm';
+import { getLastAsyncTimestamp } from '../../libs/asyncStorage/syncStorage';
+import { stopLocationTask } from '../../tasks/backgroundLocationTask';
+
+type RouteParamProps = {
+  id: string;
+};
+
+export function Arrival() {
+  const [dataNotSynced, setDataNotSynced] = useState(false);
+
+  const route = useRoute();
+  const { id } = route.params as RouteParamProps;
+
+  const realm = useRealm();
+  const { goBack } = useNavigation();
+  const historic = useObject(Historic, new BSON.UUID(id));
+
+  const title = historic?.status === 'departure' ? 'Chegada' : 'Detalhes';
+
+  function handleRemoveVehicleUsage() {
+    Alert.alert('Cancelar', 'Cancelar a utilização do veículo?', [
+      { text: 'Não', style: 'cancel' },
+      { text: 'Sim', onPress: () => removeVehicleUsage() },
+    ]);
+  }
+
+  function removeVehicleUsage() {
+    realm.write(() => {
+      realm.delete(historic);
+    });
+
+    goBack();
+  }
+
+  async function handleArrivalRegister() {
+    try {
+      if (!historic) {
+        return Alert.alert(
+          'Erro',
+          'Não foi possível obter os dados para registrar a chegada do veículo.'
+        );
+      }
+
+      await stopLocationTask();
+
+      realm.write(() => {
+        historic.status = 'arrival';
+        historic.updated_at = new Date();
+      });
+
+      Alert.alert('Chegada', 'Chegada registrada com sucesso.');
+      goBack();
+    } catch (error) {
+      Alert.alert('Erro', 'Não foi possível registar a chegada do veículo.');
+    }
+  }
+
+  useEffect(() => {
+    getLastAsyncTimestamp().then((lastSync) =>
+      setDataNotSynced(historic!.updated_at.getTime() > lastSync)
+    );
+  }, []);
+
+  return (
+    <Container>
+      <Header title={title} />
+      <Content>
+        <Label>Placa do veículo</Label>
+
+        <LicensePlate>{historic?.license_plate}</LicensePlate>
+
+        <Label>Finalidade</Label>
+
+        <Description>{historic?.description}</Description>
+      </Content>
+
+      {historic?.status === 'departure' && (
+        <Footer>
+          <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
+
+          <Button title='Registrar chegada' onPress={handleArrivalRegister} />
+        </Footer>
+      )}
+
+      {dataNotSynced && (
+        <AsyncMessage>
+          Sincronização da{' '}
+          {historic?.status === 'departure' ? 'partida' : 'chegada'} pendente
+        </AsyncMessage>
+      )}
+    </Container>
+  );
+}
